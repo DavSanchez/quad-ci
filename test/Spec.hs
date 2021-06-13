5165d91a31c98663a64bbda6c468f7450049468e
David Sánchez <david.sanchez.lt@gmail.com>
2021-06-13 02:04:27 +0100

    Running builds

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,6 +5,7 @@ import qualified Docker
 import RIO
 import qualified RIO.Map as Map
 import qualified RIO.NonEmpty.Partial as NonEmpty.Partial
+import qualified System.Process.Typed as Process
 import Test.Hspec
 
 -- Helper functions
@@ -53,6 +54,10 @@ runBuild docker build = do
 main :: IO ()
 main = hspec do
   docker <- runIO Docker.createService
-  describe "QuadCI" do
+  beforeAll cleanupDocker $ describe "QuadCI" do
     it "should run a build (success)" do
       testRunSuccess docker
+
+cleanupDocker :: IO ()
+cleanupDocker = void do
+  Process.readProcessStdout "docker rm -f $(docker ps -aq --filter \"label=quad\""
